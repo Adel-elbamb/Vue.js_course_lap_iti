61a629b3d457f436af4135362c332cd85ee5d505
Adel-elbamb <engadel987654321@gmail.com>
2025-03-10 16:42:48 +0200

    day_1

---
+++ Day_1/books.js
@@ -0,0 +1,85 @@
+const books = [
+    {
+      ISBN: '1',
+      Name: 'The Great Gatsby',
+      Category: 'Fiction',
+      Image: './images/product-item1.jpg',
+      pages: 180,
+      author: 'F. Scott Fitzgerald',
+      price: 75
+    },
+    {
+      ISBN: '2',
+      Name: 'To Kill a Mockingbird',
+      Category: 'Fiction',
+      Image: './images/product-item2.jpg',
+      pages: 281,
+      author: 'Harper Lee',
+      price: 60
+    },
+    {
+      ISBN: '3',
+      Name: 'The Catcher in the Rye',
+      Category: 'Dystopian',
+      Image: './images/product-item3.jpg',
+      pages: 328,
+      author: 'George Orwell',
+      price: 55
+    },
+    {
+      ISBN: '4',
+      Name: 'Pride and Prejudice',
+      Category: 'Romance',
+      Image: './images/product-item4.jpg',
+      pages: 30,
+      author: 'Jane Austen',
+      price: 50
+    },
+    {
+      ISBN: '5',
+      Name: 'The Catcher in the Rye',
+      Category: 'Fiction',
+      Image: './images/product-item5.jpg',
+      pages: 224,
+      author: 'J.D. Salinger',
+      price: 65
+    },
+    {
+      ISBN: '6',
+      Name: 'The Hobbit',
+      Category: 'Fantasy',
+      Image: './images/product-item6.jpg',
+      pages: 310,
+      author: 'J.R.R. Tolkien',
+      price: 70
+    },
+    {
+      ISBN: '7',
+      Name: 'The Lord of the Rings',
+      Category: 'Fantasy',
+      Image: './images/product-item7.jpg',
+      pages: 1178,
+      author: 'J.R.R. Tolkien',
+      price: 120
+    },
+    {
+      ISBN: '8',
+      Name: 'The Da Vinci Code',
+      Category: 'Mystery',
+      Image: './images/product-item8.jpg',
+      pages: 454,
+      author: 'Dan Brown',
+      price: 80
+    },
+    {
+      ISBN: '8',
+      Name: 'The Hunger Games',
+      Category: 'Science Fiction',
+      Image: './images/product-item1.jpg',
+      pages: 374,
+      author: 'Suzanne Collins',
+      price: 85
+    },
+   
+  ];
+  
